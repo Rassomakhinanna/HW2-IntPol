7f44c16be21af85beb6edbe840e1b1dae5833e2d
annarassomahina <pupkinppppp@gmail.com>
2022-09-18 17:00:40 +0400

    HW2-IntPol

--- src/Car.java
+++ src/Car.java
@@ -4,7 +4,7 @@ public class Car extends Transport {
     }
 
     public void updateTyre() {
-        System.out.println("Меняем покрышку у машины");
+        System.out.println("Меняем покрышку машины");
     }
 
     @Override
@@ -14,6 +14,6 @@ public class Car extends Transport {
     }
 
     public void checkEngine() {
-        System.out.println("Проверяем двигатель у машины");
+        System.out.println("Проверяем двигатель машины");
     }
 }
